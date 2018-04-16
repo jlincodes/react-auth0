ef03d87aa9e419f0fcdd7d04dedec2ef44e4b64a
Julie <julie.lin0812@gmail.com>
2018-04-15 23:53:42 -0700

    Secure route

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CelebrityJokes from './components/CelebrityJokes';
 import FoodJokes from './components/FoodJokes';
+import Callback from './components/Callback';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { requireAuth } from './utils/AuthService';
 
 const Root = () => {
   return (
     <div className="container">
       <Router>
         <div>
-          <Route path="/" component={FoodJokes}/>
-          <Route path="/special" component={CelebrityJokes}/>
+          <Route path="/" component={FoodJokes} />
+          <Route path="/special" component={CelebrityJokes} onEnter={requireAuth} />
+          <Route path="/callback" component={Callback} />
         </div>
       </Router>
     </div>
