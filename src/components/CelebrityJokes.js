ddd628cc8c3bde29792009759d664cd13e23c5f7
Julie <julie.lin0812@gmail.com>
2018-04-15 17:15:11 -0700

    Fix dependency issue

--- src/components/CelebrityJokes.js
+++ src/components/CelebrityJokes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import Nav from './Nav';
 import { getCelebrityData } from '../utils/chucknorris-api';
 
