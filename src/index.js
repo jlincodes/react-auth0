ca7682140d374604695b4e2a8a46d663e38c1b4f
Julie <julie.lin0812@gmail.com>
2018-04-15 17:05:29 -0700

    Add root function to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import { Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={FoodJokes}/>
+        <Route path="/special" component={CelebrityJokes}/>
+      </Router>
+    </div>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
